6adb89824f50f75c4eb597aa3f876b31283abe0c
Michael Haynes <antihaynes@gmail.com>
2024-01-05 13:52:54 -0600

    [WIP] Implement Context class

--- src/config_utils/logging/config.py
+++ src/config_utils/logging/config.py
@@ -1,13 +1,13 @@
 import logging
 import sys
 from dataclasses import Field, dataclass, field, fields
-from functools import partial, singledispatchmethod
+from functools import partial, singledispatchmethod, wraps
 from itertools import islice
 from logging import config as logging_config
 from operator import or_, setitem, truth
 from textwrap import shorten
-from typing import (Any, Callable, Dict, Generic, Iterable, Iterator, Optional,
-                    Protocol, TypeVar)
+from typing import (Any, Callable, Dict, Generic, Iterable, Iterator, Mapping,
+                    Optional, Protocol, TypeVar)
 
 from dataclasses_json import DataClassJsonMixin, config, dataclass_json
 
@@ -15,34 +15,16 @@ from ..file_utils import get_fully_qualified_name
 from .types import DataclassType, FilterType, HandlerType, LogLevel
 from .utils import get_loglevel
 
-# class ContextAwareField(Field):
-#     def __init__(
-#         self,
-#         updaters: Dict[PathLike, DictTree.Updater] = {},
-#         update_args: Dict[PathLike, Iterable[Any]] = {},
-#         **kwargs,
-#     ) -> None:
-#         kwargs.setdefault("metadata").update(
-#             {
-#                 "context": updaters,
-#             }
-#         )
-#         super().__init__(**kwargs)
-#
-#     def update_context(self, parent: DataclassType, context: DictTree) -> dict:
-#         value = getattr(parent, self.name)
-#         for path, update in self.context.get("context", {}).items():
-#             context.update_path(path, update, (value,))
-#
-#     def value(self, owner: DataclassType) -> Any:
-#         return getattr(owner, self.name)
-#
-#     def bind_parent(self, func):
-#         return map_params(func)
-#
-#
+
+@wraps(field)
+def _reference_field(*args, reference_name: str = None, **kwargs) -> Field:
+    if reference_name is not None:
+        kwargs.setdefault("metadata", {})["reference_name"] = reference_name
+    return field(*args, **kwargs)
+
+
 _callable = partial(
-    field,
+    _reference_field,
     metadata=config(
         field_name="()",
         encoder=get_fully_qualified_name,
@@ -51,34 +33,28 @@ _callable = partial(
 )
 
 _log_level = partial(
-    field,
+    _reference_field,
     metadata=config(encoder=get_loglevel),
     default=logging.NOTSET,
 )
 
-_dict_default = partial(field, default_factory=dict)
-
-# _spread_args = partial(
-#     ContextAwareField,
-#     update_functions={"..": or_},
-#     metadata=config(
-#         exclude=partial(truth, 1),
-#     ),
-# )
-#
-#
-# def _map_key(lookup_table_name: str) -> Field:
-#     return partial(
-#         ContextAwareField,
-#         update_functions={
-#             f"/{lookup_table_name}": setitem(),
-#             ".": ...,
-#         },
-#     )
+_dict_default = partial(_reference_field, default_factory=dict)
 
 
 class LoggingConfig(DataClassJsonMixin):
     """Mixin to encapsulate serialization of logging config objects."""
+
+    _context: Dict[str, Any] = field(init=False)
+
+    def __post_init__(self) -> None:
+        self._context = Context()
+        for f in fields(self):
+            val = getattr(self, f.name)
+            if isinstance(val, self.__class__):
+                self._context += val._context
+            if "udpate_context" in f.metadata:
+                updater = f.metadata["udpate_context"]
+                updater(self._context, val)
 
     def to_dict(
         self, encode_json=False, context: Optional[Dict[str, "LoggingConfig"]] = None
